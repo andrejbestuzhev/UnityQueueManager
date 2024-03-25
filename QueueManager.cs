805e00569181ca6be95aa141a317221d975c93b1
Andrej Bestuzhev <andrej@bestuzhevs.ru>
2024-03-25 16:56:31 +0100

    Init

---
+++ QueueManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets
+{
+    public class QueueManager : MonoBehaviour
+    {
+        private List<Action> Actions;
+        public static QueueManager Instance { get; private set; }
+
+
+        public void Start()
+        {
+            this.Actions = new List<Action>();
+            Instance = this;
+        }
+
+        public void Update()
+        {
+            if (this.Actions.Count > 0) {
+                for (int i = 0; i < this.Actions.Count; i++) {
+                    try {
+                        this.Actions[i].Invoke();
+                    }
+                    catch (Exception e) {
+                        this.Actions.RemoveAt(i);
+                        throw e;
+                    }
+                }
+                this.Actions.Clear();
+            }
+        }
+
+        public void Enqueue(Action action)
+        {
+            this.Actions.Add(action);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # UnityQueueManager
+
+A simple queue manager for Unity to executed needed actions in main thread.
+
+Usage:
+
+1. Add component 'QueueManager' to something global, like camera.
+2. Add following code to execute your actions:
+```
+QueueManager.Instance.Enqueue(() => {
+ // Do whatever you want!
+});
+```
